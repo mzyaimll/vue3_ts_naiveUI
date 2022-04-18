f117b08f522a1585eb6ebad72e012bd8dfb31be7
muzhengyang <2592972482@qq.com>
2022-04-18 10:38:28 +0800

    add main

---
+++ src/views/Main.vue
@@ -0,0 +1,21 @@
+<!--
+ * @Autor: GeekMzy
+ * @Date: 2022-04-18 10:34:28
+ * @LastEditors: GeekMzy
+ * @LastEditTime: 2022-04-18 10:34:50
+ * @FilePath: \vue3p\src\views\main.vue
+ * @Author: desktop-1llkr2o
+-->
+<template>
+  <div>Main vue</div>
+</template>
+
+<script>
+export default {
+  setup() {
+    return {}
+  },
+}
+</script>
+
+<style lang="scss" scoped></style>
